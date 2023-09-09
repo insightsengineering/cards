3ea89a85e0598276d2acf0c3a972f479ba8e6c0b
Daniel Sjoberg <danield.sjoberg@gmail.com>
2023-09-08 17:54:01 -0700

    small updates

--- R/ard-helpers.R
+++ R/ard-helpers.R
@@ -1,11 +1,15 @@
 #' ARD Helpers
 #'
-#' Helper functions for working with ARD data frames
+#' Helper functions for working with ARD data frames.
+#'
+#' - `get_ard_statistics()`: This function returns the statistics from an ARD
+#'   as a named list. The statistic label, formatting instruction, and any error
+#'   or warning messages are returned as attributes.
 #'
 #' @param x an ARD data frame of class 'card'
 #' @param ... optional named arguments indicating rows to subset of the ARD.
 #' For example, to return only rows where the column `"AGEGR1"` is `"65-80"`,
-#' pass `AGEGR1 = "65-80"`.
+#' pass `AGEGR1 %in% "65-80"`.
 #'
 #' @return a transformed ARD
 #' @name ard-helpers
@@ -13,17 +17,14 @@
 #' @examples
 #' ard <- ard_categorical(ADSL, by = "ARM", variables = "AGEGR1")
 #'
-#' filter_ard(ard, group1_level = "Placebo", variable_level = "65-80") |>
-#'   flatten_ard()
-#'
-#' get_ard_statistics(ard, group1_level = "Placebo", variable_level = "65-80")
+#' get_ard_statistics(ard, group1_level %in% "Placebo", variable_level %in% "65-80")
 NULL
 
 #' @export
 #' @rdname ard-helpers
 get_ard_statistics <- function(x, ...) {
   # subset the ARD
-  ard_subset <- filter_ard(x, ...)
+  ard_subset <- dplyr::filter(x, ...)
 
   # return a named list of the selected stats
   # add attributes for the label, formatting function, warnings, and errors
@@ -41,42 +42,4 @@ get_ard_statistics <- function(x, ...) {
     ) |>
     stats::setNames(ard_subset[["stat_name"]])
 }
-
-#' @export
-#' @rdname ard-helpers
-filter_ard <- function(x, ...) {
-  # save dots as named list of expressions -------------------------------------
-  dots <- rlang::enexprs(...)
-
-  # check inputs ---------------------------------------------------------------
-  if (!inherits(x, "card")) {
-    cli::cli_abort("Argument {.code x} must be class {.cls card}.")
-  }
-  if (!rlang::is_named2(dots)) {
-    cli::cli_abort("Arguments passed in {.code ...} must be named.")
-  }
-  if (rlang::is_empty(dots)) return(x)
-
-  # construct calls to dplyr::filter() -----------------------------------------
-  filter_args <-
-    .mapply(
-      FUN = function(value, colname) {
-        if (is.list(x[[colname]])) {
-          rlang::expr(
-            unlist(lapply(
-              X = !!rlang::data_sym(colname),
-              FUN = function(p) ifelse(rlang::is_empty(p), FALSE, p %in% !!value)
-            ))
-          )
-        }
-        rlang::expr(!!rlang::data_sym(colname) %in% !!value)
-      },
-      dots = list(dots, names(dots)),
-      MoreArgs = NULL
-    )
-
-  # filter ard data set and return ---------------------------------------------
-  dplyr::filter(x, !!!filter_args)
-}
-
 
