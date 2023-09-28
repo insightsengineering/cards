c390a5f265c801933d7d1469abbf27d265560e3f
Daniel Sjoberg <danield.sjoberg@gmail.com>
2023-09-28 09:11:56 -0700

    adding a few other basic tests

--- R/ard_comparison.R
+++ R/ard_comparison.R
@@ -18,6 +18,15 @@
 #'   dplyr::filter(ARM %in% c("Placebo", "Xanomeline High Dose")) |>
 #'   ard_wilcoxtest(by = "ARM", variable = "AGE") |>
 #'   flatten_ard()
+#'
+#'
+#' ADSL |>
+#'   ard_chisqtest(by = "ARM", variable = "AGEGR1") |>
+#'   flatten_ard()
+#'
+#' ADSL[1:30,] |>
+#'   ard_fishertest(by = "ARM", variable = "AGEGR1") |>
+#'   flatten_ard()
 NULL
 
 #' @rdname ard_comparison
@@ -31,6 +40,7 @@ ard_ttest <- function(data, by, variable, ...) {
   check_not_missing(variable, "variable")
   check_not_missing(by, "by")
   check_class_data_frame(data = data)
+  .process_selecting_args(data, by = {{ by }}, variable = {{ variable }})
 
   # perform t-test and format results ------------------------------------------
   lst_ttest <- eval_capture_conditions(stats::t.test(data[[variable]] ~ data[[by]], ...))
@@ -103,6 +113,7 @@ ard_wilcoxtest <- function(data, by, variable, ...) {
   check_not_missing(variable, "variable")
   check_not_missing(by, "by")
   check_class_data_frame(data = data)
+  .process_selecting_args(data, by = {{ by }}, variable = {{ variable }})
 
   # perform Wilcoxon test and format results -----------------------------------
   lst_wilcox <- eval_capture_conditions(stats::wilcox.test(data[[variable]] ~ data[[by]], ...))
@@ -155,6 +166,144 @@ ard_wilcoxtest <- function(data, by, variable, ...) {
       context = "wilcoxtest",
       warning = lst_wilcox["warning"],
       error = lst_wilcox["error"]
+    ) %>%
+    structure(., class = c("card", class(.)))
+}
+
+
+#' @rdname ard_comparison
+#' @export
+ard_chisqtest <- function(data, by, variable, ...) {
+  # check installed packages ---------------------------------------------------
+  rlang::check_installed("broom")
+
+  # check inputs ---------------------------------------------------------------
+  check_not_missing(data, "data")
+  check_not_missing(variable, "variable")
+  check_not_missing(by, "by")
+  check_class_data_frame(data = data)
+  .process_selecting_args(data, by = {{ by }}, variable = {{ variable }})
+
+  # perform chisq test and format results -----------------------------------
+  lst_chisq <-
+    eval_capture_conditions(stats::chisq.test(x = data[[variable]], y = data[[by]], ...))
+
+  # if there are results, put them in the ARD format ---------------------------
+  if (!is.null(lst_chisq[["result"]])) {
+    # additional args passed by user (and default values) will appended to broom::tidy() results
+    df_chisqtest_args <-
+      utils::modifyList(
+        # grab the default arg values
+        x = formals(stats::chisq.test) %>%
+          `[`(c("correct", "p", "rescale.p", "simulate.p.value", "B")),
+        # update with any values passed by the user
+        val = rlang::dots_list(...),
+        keep.null = FALSE
+      ) |>
+      lapply(function(x) list(x)) |>
+      dplyr::as_tibble()
+
+    ret <-
+      broom::tidy(lst_chisq[["result"]]) |>
+      dplyr::mutate(
+        dplyr::across(everything(), .fns = list),
+        group1 = .env$by,
+        variable = .env$variable
+      ) |>
+      dplyr::bind_cols(df_chisqtest_args) |>
+      tidyr::pivot_longer(
+        cols = -c("group1", "variable"),
+        names_to = "stat_name",
+        values_to = "statistic"
+      )
+  }
+
+  # if there was an error, return empty data frame in ARD format ---------------
+  else {
+    ret <-
+      dplyr::tibble(
+        group1 = .env$by,
+        variable = .env$variable,
+        stat_name = NA_character_,
+        statistic = list(NULL)
+      )
+  }
+
+  # return and add warning/errors ----------------------------------------------
+  ret |>
+    dplyr::mutate(
+      context = "chisqtest",
+      warning = lst_chisq["warning"],
+      error = lst_chisq["error"]
+    ) %>%
+    structure(., class = c("card", class(.)))
+}
+
+#' @rdname ard_comparison
+#' @export
+ard_fishertest <- function(data, by, variable, ...) {
+  # check installed packages ---------------------------------------------------
+  rlang::check_installed("broom")
+
+  # check inputs ---------------------------------------------------------------
+  check_not_missing(data, "data")
+  check_not_missing(variable, "variable")
+  check_not_missing(by, "by")
+  check_class_data_frame(data = data)
+  .process_selecting_args(data, by = {{ by }}, variable = {{ variable }})
+
+  # perform fisher test and format results -----------------------------------
+  lst_fisher <-
+    eval_capture_conditions(stats::fisher.test(x = data[[variable]], y = data[[by]], ...))
+
+  # if there are results, put them in the ARD format ---------------------------
+  if (!is.null(lst_fisher[["result"]])) {
+    # additional args passed by user (and default values) will appended to broom::tidy() results
+    df_fishertest_args <-
+      utils::modifyList(
+        # grab the default arg values
+        x = formals(stats::fisher.test) %>%
+          `[`(c("workspace", "hybrid", "hybridPars", "control", "or",
+                "conf.int", "conf.level", "simulate.p.value", "B")),
+        # update with any values passed by the user
+        val = rlang::dots_list(...),
+        keep.null = FALSE
+      ) |>
+      lapply(function(x) list(x)) |>
+      dplyr::as_tibble()
+
+    ret <-
+      broom::tidy(lst_fisher[["result"]]) |>
+      dplyr::mutate(
+        dplyr::across(everything(), .fns = list),
+        group1 = .env$by,
+        variable = .env$variable
+      ) |>
+      dplyr::bind_cols(df_fishertest_args) |>
+      tidyr::pivot_longer(
+        cols = -c("group1", "variable"),
+        names_to = "stat_name",
+        values_to = "statistic"
+      )
+  }
+
+  # if there was an error, return empty data frame in ARD format ---------------
+  else {
+    ret <-
+      dplyr::tibble(
+        group1 = .env$by,
+        variable = .env$variable,
+        stat_name = NA_character_,
+        statistic = list(NULL)
+      )
+  }
+
+  # return and add warning/errors ----------------------------------------------
+  ret |>
+    dplyr::mutate(
+      context = "fishertest",
+      warning = lst_fisher["warning"],
+      error = lst_fisher["error"]
     ) %>%
     structure(., class = c("card", class(.)))
 }
