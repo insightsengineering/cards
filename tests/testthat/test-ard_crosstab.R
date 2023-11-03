6956beabf10bd4c9044660af3255996b2dcc69f5
Daniel Sjoberg <danield.sjoberg@gmail.com>
2023-11-02 21:03:15 -0700

    Update test-ard_crosstab.R

--- tests/testthat/test-ard_crosstab.R
+++ tests/testthat/test-ard_crosstab.R
@@ -4,6 +4,26 @@ test_that("ard_crosstab works", {
     NA
   )
 
+  mtrx_conts <- with(ADSL, table(AGEGR1, ARM)) |> unclass()
+  mtrx_percs <- mtrx_conts / sum(mtrx_conts)
+
+  expect_equal(
+    ard_crosstab |>
+      dplyr::filter(group1_level %in% "Placebo", variable_level %in% "<65", stat_name %in% "n") |>
+      dplyr::pull(statistic) |>
+      unlist(),
+    mtrx_conts["<65", "Placebo"]
+  )
+
+  expect_equal(
+    ard_crosstab |>
+      dplyr::filter(group1_level %in% "Placebo", variable_level %in% "<65", stat_name %in% "p") |>
+      dplyr::pull(statistic) |>
+      unlist(),
+    mtrx_percs["<65", "Placebo"]
+  )
+
+
   expect_equal(
     ard_crosstab(
       mtcars,
