5268a4b19742e51f224b25a6439055c49eafd7db
Daniel Sjoberg <danield.sjoberg@gmail.com>
2023-12-03 08:28:48 -0800

    added fmt_fn argument (#58)
    closes #38

--- tests/testthat/test-ard_continuous.R
+++ tests/testthat/test-ard_continuous.R
@@ -27,6 +27,28 @@ test_that("ard_continuous() works", {
   )
 })
 
+
+test_that("ard_continuous(fmt_fn) argument works", {
+  ard_continuous(
+    ADSL,
+    variables = "AGE",
+    statistics = list(AGE = continuous_variable_summary_fns(c("N", "mean", "median"))),
+    fmt_fn =
+      list(
+        AGE =
+          list(
+            mean = function(x) round(x, digits = 3) |> as.character(),
+            N = function(x) format(round(x, digits = 2), nsmall = 2),
+            N_obs = function(x) format(round(x, digits = 2), nsmall = 2)
+          )
+      )
+  ) |>
+    apply_statistic_fmt_fn() |>
+    dplyr::select(variable, stat_name, statistic, statistic_fmt) |>
+    as.data.frame() |>
+    expect_snapshot()
+})
+
 test_that("ard_continuous() messaging", {
   # proper error message when statistics argument mis-specified
   expect_snapshot(
