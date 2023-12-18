d9df678f6193770befd1f7a2d84654a0ce6aa539
Daniel Sjoberg <danield.sjoberg@gmail.com>
2023-12-18 07:30:58 -0800

    in progress

--- R/ard_crosstab.R
+++ R/ard_crosstab.R
@@ -5,42 +5,82 @@
 #'
 #' @inheritParams ard_categorical
 #' @param by ([`tidy-select`][dplyr::dplyr_tidy_select])\cr
-#'   columns to compute statistics by.
+#'   a single column to compute statistics by.
+#' @param percent (`string`)\cr
+#'   one of `c("cell", "column", "row")` that determines how the percentage is
+#'   calculated. Default is `"cell"`
 #'
 #' @return a data frame
 #' @export
 #'
 #' @examples
 #' ard_crosstab(ADSL, variables = "AGEGR1", by = "ARM")
-ard_crosstab <- function(data, variables, by) {
-  # check inputs ---------------------------------------------------------------
-  check_not_missing(data, "data")
-  check_not_missing(variables, "variables")
-  check_not_missing(by, "by")
-  check_class_data_frame(data = data)
-
+ard_crosstab <- function(data, variables, by,
+                         statistics = everything() ~ categorical_variable_summary_fns(),
+                         percent = c("cell", "column", "row")) {
   # process arguments ----------------------------------------------------------
-  data <- dplyr::ungroup(data)
+  percent <- rlang::arg_match(percent)
+  check_not_missing(data)
+  check_not_missing(variables)
+  check_not_missing(by)
+  check_class_data_frame(data = data)
   process_selectors(data, variables = {{ variables }}, by = {{ by }})
+  check_length(by, length = 1L)
 
   # return empty tibble if no variables selected -------------------------------
   if (rlang::is_empty(variables)) return(dplyr::tibble())
 
+  # calculate summary stats ----------------------------------------------------
+  switch(
+    percent,
+    "column" =
+      .ard_crosstab_column(data = data, variables = variables,
+                        by = by, statistics = statistics),
+    "row" =
+      .ard_crosstab_row(data = data, variables = variables,
+                        by = by, statistics = statistics),
+    "cell" =
+      .ard_crosstab_cell(data = data, variables = variables,
+                         by = by, statistics = statistics)
+  ) |>
+    dplyr::mutate(context = "crosstab")
+}
+
+.ard_crosstab_column <- function(data, variables, by, statistics) {
+  ard_categorical(data, variables = all_of(variables), by = all_of(by))
+}
+
+.ard_crosstab_row <- function(data, variables, by, statistics) {
+  # tabulate crosstab ----------------------------------------------------------
+  lapply(
+    variables,
+    FUN = function(variable) {
+      ard_categorical(data, variables = all_of(by), by = all_of(variable))
+    }
+  ) |>
+    dplyr::bind_rows() |>
+    dplyr::rename(group1 = "variable",
+                  group1_level = "variable_level",
+                  variable = "group1",
+                  variable_level = "group1_level")
+}
+
+.ard_crosstab_cell <- function(data, variables, by, statistics) {
   # tabulate crosstab ----------------------------------------------------------
   lapply(
     variables,
     FUN = function(variable) {
       denominator <-
-        data |>
-        nest_for_ard(by = by, rename_columns = FALSE) |>
+        data[by] |>
+        dplyr::distinct() |>
         dplyr::mutate(
-          data = .env$data[c(by, variable)] |> tidyr::drop_na() |> dplyr::select(all_of(variable)) |> list()
+          ...ard_denom_n... =
+            data |> tidyr::drop_na(all_of(c(by, variable))) |> nrow()
         ) |>
-        tidyr::unnest(cols = dplyr::everything())
+        tidyr::uncount(weights = .data$...ard_denom_n...)
 
-      ard_categorical(data = data, variables = all_of(variable), by = all_of(by), denominator = denominator)
+      ard_categorical(data, variables = all_of(variable), by = all_of(by), denominator = denominator)
     }
   ) |>
-    dplyr::bind_rows() |>
-    dplyr::mutate(context = "crosstab")
+    dplyr::bind_rows()
 }
