1851de605ad0cb49cdc1145412938dbb8341749a
Daniel Sjoberg <danield.sjoberg@gmail.com>
2023-12-20 11:41:09 -0800

    Updated `tbl_cross()` with `percent` argument  (#84)
    **What changes are proposed in this pull request?**
    * The `tbl_cross()` function now calculates percentages in three ways;
    column, cell, and row.
    * Restricted the `by` variable to be one column, so the percentages will
    always make sense.
    
    **Reference GitHub issue associated with pull request.** _e.g., 'closes
    #1'_
    closes #56 
    
    
    --------------------------------------------------------------------------------
    
    Reviewer Checklist (if item does not apply, mark is as complete)
    
    - [x] Ensure all package dependencies are installed:
    `devtools::install_dev_deps()`
    - [x] PR branch has pulled the most recent updates from master branch:
    `usethis::pr_merge_main()`
    - [x] If a bug was fixed, a unit test was added.
    - [x] Run `pkgdown::build_site()`. Check the R console for errors, and
    review the rendered website.
    - [x] Code coverage is suitable for any new functions/features:
    `devtools::test_coverage()`
    - [x] `usethis::use_spell_check()` runs with no spelling errors in
    documentation
    
    When the branch is ready to be merged:
    - [ ] Update `NEWS.md` with the changes from this pull request under the
    heading "`# cards (development version)`". If there is an issue
    associated with the pull request, reference it in parentheses at the end
    update (see `NEWS.md` for examples).
    - [ ] Increment the version number using `usethis::use_version(which =
    "dev")`
    - [ ] Run `usethis::use_spell_check()` again
    - [ ] Approve Pull Request
    - [ ] Merge the PR. Please use "Squash and merge".

--- tests/testthat/test-ard_crosstab.R
+++ tests/testthat/test-ard_crosstab.R
@@ -1,4 +1,4 @@
-test_that("ard_crosstab works", {
+test_that("ard_crosstab(percent='cell') works", {
   expect_error(
     ard_crosstab <- ard_crosstab(ADSL, variables = "AGEGR1", by = "ARM"),
     NA
@@ -31,5 +31,78 @@ test_that("ard_crosstab works", {
       variables = starts_with("xxxxx")
     ),
     dplyr::tibble()
+  )
+})
+
+test_that("ard_crosstab(percent='row') works", {
+  expect_error(
+    ard_crosstab_row <- ard_crosstab(ADSL, variables = "AGEGR1", by = "ARM", percent = "row"),
+    NA
+  )
+
+  xtab_count <- with(ADSL, table(AGEGR1, ARM))
+  xtab_percent <- proportions(xtab_count, margin = 1)
+
+  expect_equal(
+    xtab_count[rownames(xtab_count) %in% "<65", colnames(xtab_count) %in% "Placebo"],
+    ard_crosstab_row |>
+      dplyr::filter(variable_level %in% "<65", group1_level %in% "Placebo", stat_name %in% "n") |>
+      dplyr::pull(statistic) |>
+      unlist(),
+    ignore_attr = TRUE
+  )
+  expect_equal(
+    xtab_percent[rownames(xtab_percent) %in% "<65", colnames(xtab_percent) %in% "Placebo"],
+    ard_crosstab_row |>
+      dplyr::filter(variable_level %in% "<65", group1_level %in% "Placebo", stat_name %in% "p") |>
+      dplyr::pull(statistic) |>
+      unlist(),
+    ignore_attr = TRUE
+  )
+
+  expect_equal(
+    xtab_count[rownames(xtab_count) %in% ">80", colnames(xtab_count) %in% "Xanomeline Low Dose"],
+    ard_crosstab_row |>
+      dplyr::filter(variable_level %in% ">80", group1_level %in% "Xanomeline Low Dose", stat_name %in% "n") |>
+      dplyr::pull(statistic) |>
+      unlist(),
+    ignore_attr = TRUE
+  )
+  expect_equal(
+    xtab_percent[rownames(xtab_percent) %in% ">80", colnames(xtab_percent) %in% "Xanomeline Low Dose"],
+    ard_crosstab_row |>
+      dplyr::filter(variable_level %in% ">80", group1_level %in% "Xanomeline Low Dose", stat_name %in% "p") |>
+      dplyr::pull(statistic) |>
+      unlist(),
+    ignore_attr = TRUE
+  )
+
+  # testing the arguments work properly
+  expect_error(
+    ard_with_args <-
+      ard_crosstab(
+        ADSL, variables = "AGEGR1", by = "ARM",
+        percent = "row",
+        statistics = list(AGEGR1 = categorical_variable_summary_fns(c("n", "N"))),
+        fmt_fn = list(AGEGR1 = list("n" = 2))
+      ),
+    NA
+  )
+
+  expect_snapshot(
+    ard_with_args |>
+      apply_statistic_fmt_fn() |>
+      flatten_ard() |>
+      dplyr::select(-statistic_fmt_fn, -warning, -error) |>
+      as.data.frame()
+  )
+})
+
+test_that("ard_crosstab(percent='column') works", {
+  expect_equal(
+    ard_crosstab(ADSL, variables = "AGEGR1", by = "ARM", percent = "column") |>
+      dplyr::select(all_ard_groups(), all_ard_variables(), stat_name, statistic),
+    ard_categorical(ADSL, variables = "AGEGR1", by = "ARM") |>
+      dplyr::select(all_ard_groups(), all_ard_variables(), stat_name, statistic)
   )
 })
