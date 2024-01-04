0847f7b142954a9f11ed082a8509717db138bf0d
Daniel Sjoberg <danield.sjoberg@gmail.com>
2024-01-03 20:13:37 -0800

    selector updates (#100)
    **What changes are proposed in this pull request?**
    
    
    **Reference GitHub issue associated with pull request.** _e.g., 'closes
    #1'_
    closes #97 
    closes #98 
    
    
    --------------------------------------------------------------------------------
    
    Reviewer Checklist (if item does not apply, mark is as complete)
    
    - [ ] Ensure all package dependencies are installed:
    `devtools::install_dev_deps()`
    - [ ] PR branch has pulled the most recent updates from master branch:
    `usethis::pr_merge_main()`
    - [ ] If a bug was fixed, a unit test was added.
    - [ ] Run `pkgdown::build_site()`. Check the R console for errors, and
    review the rendered website.
    - [ ] Code coverage is suitable for any new functions/features:
    `devtools::test_coverage()`
    - [ ] `usethis::use_spell_check()` runs with no spelling errors in
    documentation
    
    When the branch is ready to be merged:
    - [ ] Update `NEWS.md` with the changes from this pull request under the
    heading "`# cards (development version)`". If there is an issue
    associated with the pull request, reference it in parentheses at the end
    update (see `NEWS.md` for examples).
    - [ ] Increment the version number using `usethis::use_version(which =
    "dev")`
    - [ ] Run `usethis::use_spell_check()` again
    - [ ] Approve Pull Request
    - [ ] Merge the PR. Please use "Squash and merge".

--- R/selectors.R
+++ R/selectors.R
@@ -11,6 +11,10 @@
 #'   as `dplyr::select()`. Function selects variables columns, e.g. columns
 #'   named `"variable"` or `"variable_level"`.
 #'
+#' @param variables logical indicating whether to select the columns with the
+#' variable names. Default is `TRUE`
+#' @param levels logical indicating whether to select the columns with the
+#' variable levels. Default is `TRUE`
 #' @return tidyselect output
 #' @name selectors
 #'
@@ -22,12 +26,22 @@ NULL
 
 #' @export
 #' @rdname selectors
-all_ard_groups <- function() {
-  dplyr::matches("^group[0-9]+$|^group[0-9]+_level$")
+all_ard_groups <- function(variables = TRUE, levels = TRUE) {
+  if (isTRUE(variables) && isTRUE(levels))
+    return(dplyr::matches("^group[0-9]+$|^group[0-9]+_level$"))
+  if (isTRUE(variables))
+    return(dplyr::matches("^group[0-9]+$$"))
+  if (isTRUE(levels))
+    return(dplyr::matches("^group[0-9]+_level$"))
 }
 
 #' @export
 #' @rdname selectors
-all_ard_variables <- function() {
-  dplyr::any_of(c("variable", "variable_level"))
+all_ard_variables <- function(variables = TRUE, levels = TRUE) {
+  if (isTRUE(variables) && isTRUE(levels))
+    return(dplyr::any_of(c("variable", "variable_level")))
+  if (isTRUE(variables))
+    return(dplyr::any_of("variable"))
+  if (isTRUE(levels))
+    return(dplyr::any_of("variable_level"))
 }
