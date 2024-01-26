8004bdd506cf29e2e77abb7c12729916788122c7
Daniel Sjoberg <danield.sjoberg@gmail.com>
2024-01-26 08:20:23 -0800

    adding `print_ard_conditions()` (#120)
    **What changes are proposed in this pull request?**
    Adds the `print_ard_conditions()` function
    
    **Reference GitHub issue associated with pull request.** _e.g., 'closes
    #1'_
    closes #118 
    
    <img width="850" alt="image"
    src="https://github.com/insightsengineering/cards/assets/26774684/bb62e550-734f-45a3-b149-8488ef3e3816">
    
    
    
    --------------------------------------------------------------------------------
    
    Reviewer Checklist (if item does not apply, mark is as complete)
    
    - [ ] Ensure all package dependencies are installed:
    `devtools::install_dev_deps()`
    - [ ] PR branch has pulled the most recent updates from master branch:
    `usethis::pr_merge_main()`
    - [ ] If a bug was fixed, a unit test was added.
    - [ ] Run `pkgdown::build_site()`. Check the R console for errors, and
    review the rendered website.
    - [ ] Code coverage is suitable for any new functions/features:
    `devtools::test_coverage()`
    - [ ] `usethis::use_spell_check()` runs with no spelling errors in
    documentation
    
    When the branch is ready to be merged:
    - [ ] Update `NEWS.md` with the changes from this pull request under the
    heading "`# cards (development version)`". If there is an issue
    associated with the pull request, reference it in parentheses at the end
    update (see `NEWS.md` for examples).
    - [ ] Increment the version number using `usethis::use_version(which =
    "dev")`
    - [ ] Run `usethis::use_spell_check()` again
    - [ ] Approve Pull Request
    - [ ] Merge the PR. Please use "Squash and merge".
    
    ---------
    
    Signed-off-by: Daniel Sjoberg <danield.sjoberg@gmail.com>

---
+++ tests/testthat/test-print_ard_conditions.R
@@ -0,0 +1,43 @@
+test_that("print_ard_conditions() works", {
+  # nothing prints with no errors/warnings
+  expect_snapshot(
+    ard_continuous(ADSL, variables = AGE) |>
+      print_ard_conditions()
+  )
+
+  # expected messaging without by variable
+  expect_snapshot(
+    ard_continuous(
+      ADSL,
+      variables = AGE,
+      statistics = ~list(
+        mean = \(x) mean(x),
+        mean_warning = \(x) {warning("warn1"); warning("warn2"); mean(x)},
+        err_fn = \(x) stop("'tis an error")
+      )
+    ) |>
+      print_ard_conditions()
+  )
+
+  # expected messaging with by variable
+  expect_snapshot(
+    ard_continuous(
+      ADSL,
+      variables = AGE,
+      by = ARM,
+      statistics = ~list(
+        mean = \(x) mean(x),
+        mean_warning = \(x) {warning("warn1"); warning("warn2"); mean(x)},
+        err_fn = \(x) stop("'tis an error")
+      )
+    ) |>
+      print_ard_conditions()
+  )
+
+  # expected messaging when the same error appears for all stats (consolidated correctly)
+  expect_snapshot(
+    ard_continuous(ADSL, variables = AGE) |>
+      dplyr::mutate(error = list("repeated error")) |>
+      print_ard_conditions()
+  )
+})
