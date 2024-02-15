fd410f81bd11f1338e8f0e6c2fc235575d7f65af
Becca Krouse <becca.z.krouse@gsk.com>
2024-02-15 11:56:30 -0500

    create ard_stack

---
+++ R/ard_stack.R
@@ -0,0 +1,130 @@
+#' Stack ARDs
+#'
+#' Stack multiple ARD calls sharing common input `data` and `by` variables.
+#' Optionally incorporate additional information on represented variables (i.e.
+#' big N's, missingness, attributes) and/or tidy for use in displays with
+#' `shuffle_ard()`.
+#'
+#' @param data (`data.frame`)\cr
+#'   a data frame
+#' @param by ([`tidy-select`][dplyr::dplyr_tidy_select])\cr
+#'   columns to tabulate by in the series of ARD function calls
+#' @param ... ([`dynamic-dots`][dyn-dots])\cr
+#'   Series of ARD function calls to be run and stacked
+#' @param .compute_by_ns (`logical`)\cr
+#'   logical indicating whether to compute big Ns for the `by` variable
+#'   Default is `TRUE`.
+#' @param .compute_missing (`logical`)\cr
+#'   logical indicating whether to include the results of `ard_missing()` for all
+#'   variables represented in the ARD. Default is `FALSE`.
+#' @param .add_attributes (`logical`)\cr
+#'   logical indicating whether to include the results of `ard_attributes()` for all
+#'   variables represented in the ARD. Default is `FALSE`.
+#' @param .shuffle (`logical`)\cr
+#'   logical indicating whether to perform `shuffle_ard()` on the final result.
+#'   Default is `FALSE`.
+#' @param .trim (`logical`)\cr
+#'   logical indicating whether to set `trim = TRUE` in the call to `shuffle_ard()`.
+#'   Default is `TRUE`. Will be ignored if `.shuffle = FALSE`.
+#'
+#' @return  a transformed ARD data frame (of class 'card' if `.shuffle = FALSE`)
+#'
+#' @export
+#' @examples
+#'
+#' ard_stack(
+#'   data = ADSL,
+#'   by = "ARM",
+#'   ard_categorical(variables = "AGEGR1"),
+#'   ard_continuous(variables = "AGE")
+#' )
+#'
+#' ard_stack(
+#'   data = ADSL,
+#'   by = "ARM",
+#'   ard_categorical(variables = "AGEGR1"),
+#'   ard_continuous(variables = "AGE"),
+#'   .shuffle = TRUE
+#' )
+#'
+ard_stack <- function(data,
+                      by = NULL,
+                      ...,
+                      .compute_by_ns = TRUE,
+                      .compute_missing = FALSE,
+                      .add_attributes = FALSE,
+                      .shuffle = FALSE,
+                      .trim = TRUE) {
+  # capture quosures -----------------------------------------------------------
+  dots <- enquos(...)
+  by <- enquo(by)
+
+  # check inputs ---------------------------------------------------------------
+  check_not_missing(data)
+  check_class_data_frame(x = data)
+
+  check_class(.compute_by_ns, "logical")
+  check_scalar(.compute_by_ns)
+  check_class(.compute_missing, "logical")
+  check_scalar(.compute_missing)
+  check_class(.add_attributes, "logical")
+  check_scalar(.add_attributes)
+  check_class(.shuffle, "logical")
+  check_scalar(.shuffle)
+  check_class(.trim, "logical")
+  check_scalar(.trim)
+
+  # evaluate the dots using common `data` and `by`
+  ard_list <- lapply(
+    dots,
+    function(x) {
+      x_rhs <- f_rhs(x)
+      x_fn <- call_name(x_rhs)
+      x_args <- call_args(x_rhs)
+      do.call(x_fn, c(list(data = data, by = by), x_args), envir = attr(x, ".Environment"))
+    }
+  )
+
+  # compute Ns by group / combine main calls
+  if (isTRUE(.compute_by_ns) && !quo_is_null(by)) {
+    ard_full <- bind_ard(
+      ard_list,
+      ard_categorical(
+        data = data,
+        variables = {{ by }},
+        statistics = everything() ~ categorical_variable_summary_fns("N")
+      )
+    )
+  } else {
+    ard_full <- bind_ard(ard_list, .update = TRUE)
+  }
+
+  # get all variables represented
+  variables <- unique(ard_full$variable)
+
+  # missingness
+  if (isTRUE(.compute_missing)) {
+    ard_full <- bind_ard(
+      ard_full,
+      ard_missing(data = data, variables = variables)
+    )
+  }
+
+  # attributes
+  if (isTRUE(.add_attributes)) {
+    ard_full <- bind_ard(
+      ard_full,
+      ard_attributes(data = data, variables = variables)
+    )
+  }
+
+  # order
+  ard_full <- tidy_ard_row_order(ard_full)
+
+  # shuffle
+  if (isTRUE(.shuffle)) {
+    return(shuffle_ard(ard_full, trim = .trim))
+  }
+
+  ard_full
+}
---
+++ tests/testthat/test-ard_stack.R
@@ -0,0 +1,132 @@
+test_that("ard_stack() works", {
+  # with by variable
+  expect_error(
+    ard1 <- ard_stack(
+      data = mtcars,
+      by = "cyl",
+      ard_continuous(variables = "mpg"),
+      ard_dichotomous(variables = "vs")
+    ),
+    NA
+  )
+
+  expect_equal(
+    ard1,
+    bind_ard(
+      ard_continuous(data = mtcars, by = "cyl", variables = "mpg"),
+      ard_dichotomous(data = mtcars, by = "cyl", variables = "vs"),
+      ard_categorical(data = mtcars, variables = "cyl", statistics = everything() ~ categorical_variable_summary_fns("N")),
+      .order = TRUE
+    ),
+    ignore_function_env = TRUE
+  )
+
+  # check equivalency NSE
+  expect_equal(
+    ard1,
+    ard_stack(
+      data = mtcars,
+      by = cyl,
+      ard_continuous(variables = mpg),
+      ard_dichotomous(variables = vs)
+    ),
+    ignore_function_env = TRUE
+  )
+
+  # without by variable
+  expect_error(
+    ard2 <- ard_stack(
+      data = mtcars,
+      by = NULL,
+      ard_continuous(variables = "mpg"),
+      ard_dichotomous(variables = "vs")
+    ),
+    NA
+  )
+
+  expect_equal(
+    ard2,
+    bind_ard(
+      ard_continuous(data = mtcars, variables = "mpg"),
+      ard_dichotomous(data = mtcars, variables = "vs"),
+      .order = TRUE
+    ),
+    ignore_function_env = TRUE
+  )
+
+  # with by variable but no extra by variable big Ns
+  expect_error(
+    ard3 <- ard_stack(
+      data = mtcars,
+      by = "cyl",
+      ard_continuous(variables = "mpg"),
+      ard_dichotomous(variables = "vs"),
+      .compute_by_ns = FALSE
+    ),
+    NA
+  )
+
+  expect_equal(
+    ard3,
+    bind_ard(
+      ard_continuous(data = mtcars, by = "cyl", variables = "mpg"),
+      ard_dichotomous(data = mtcars, by = "cyl", variables = "vs"),
+      .order = TRUE
+    ),
+    ignore_function_env = TRUE
+  )
+})
+
+test_that("ard_stack() adding missing/attributes", {
+  expect_error(
+    ard_test <- ard_stack(
+      data = mtcars,
+      by = "cyl",
+      ard_continuous(variables = "mpg"),
+      ard_dichotomous(variables = "vs"),
+      .compute_missing = TRUE,
+      .add_attributes = TRUE
+    ),
+    NA
+  )
+
+  expect_equal(
+    ard_test |>
+      dplyr::select(-statistic_fmt_fn),
+    bind_ard(
+      ard_continuous(data = mtcars, by = "cyl", variables = "mpg"),
+      ard_dichotomous(data = mtcars, by = "cyl", variables = "vs"),
+      ard_categorical(data = mtcars, variables = "cyl", statistics = everything() ~ categorical_variable_summary_fns("N")),
+      ard_missing(data = mtcars, variables = c("cyl", "mpg", "vs")),
+      ard_attributes(data = mtcars, variables = c("cyl", "mpg", "vs")),
+      .update = TRUE,
+      .order = TRUE
+    ) |>
+      dplyr::select(-statistic_fmt_fn)
+  )
+})
+
+
+test_that("ard_stack() .shuffle argument", {
+  expect_error(
+    ard_test <- ard_stack(
+      data = mtcars,
+      by = "cyl",
+      ard_continuous(variables = "mpg"),
+      ard_dichotomous(variables = "vs"),
+      .shuffle = TRUE
+    ),
+    NA
+  )
+
+  expect_equal(
+    ard_test,
+    bind_ard(
+      ard_continuous(data = mtcars, by = "cyl", variables = "mpg"),
+      ard_dichotomous(data = mtcars, by = "cyl", variables = "vs"),
+      ard_categorical(data = mtcars, variables = "cyl", statistics = everything() ~ categorical_variable_summary_fns("N")),
+      .order = TRUE
+    ) |>
+      shuffle_ard()
+  )
+})
