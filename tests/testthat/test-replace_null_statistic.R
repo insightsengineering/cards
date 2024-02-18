4ba610ef9090163dfffbac7b125c71e104ab427a
Daniel Sjoberg <danield.sjoberg@gmail.com>
2024-02-18 14:04:37 -0800

    adding `replace_null_statistic()` (#186)
    **What changes are proposed in this pull request?**
    * Added the `replace_null_statistic()` function. (#185)
    
    **Reference GitHub issue associated with pull request.** _e.g., 'closes
    #<issue number>'_
    closes #185 
    
    
    --------------------------------------------------------------------------------
    
    Pre-review Checklist (if item does not apply, mark is as complete)
    - [ ] **All** GitHub Action workflows pass with a :white_check_mark:
    - [ ] PR branch has pulled the most recent updates from master branch:
    `usethis::pr_merge_main()`
    - [ ] If a bug was fixed, a unit test was added.
    - [ ] Code coverage is suitable for any new functions/features
    (generally, 100% coverage for new code): `devtools::test_coverage()`
    - [ ] Request a reviewer
    
    Reviewer Checklist (if item does not apply, mark is as complete)
    
    - [ ] If a bug was fixed, a unit test was added.
    - [ ] Run `pkgdown::build_site()`. Check the R console for errors, and
    review the rendered website.
    - [ ] Code coverage is suitable for any new functions/features:
    `devtools::test_coverage()`
    
    When the branch is ready to be merged:
    - [ ] Update `NEWS.md` with the changes from this pull request under the
    heading "`# cards (development version)`". If there is an issue
    associated with the pull request, reference it in parentheses at the end
    update (see `NEWS.md` for examples).
    - [ ] **All** GitHub Action workflows pass with a :white_check_mark:
    - [ ] Approve Pull Request
    - [ ] Merge the PR. Please use "Squash and merge" or "Rebase and merge".

---
+++ tests/testthat/test-replace_null_statistic.R
@@ -0,0 +1,18 @@
+test_that("replace_null_statistic() works", {
+  expect_error(
+    ard_with_missing_stats <-
+      data.frame(x = rep_len(NA_character_, 10)) |>
+      ard_continuous(
+        variables = x,
+        statistics = ~ continuous_variable_summary_fns(c("median", "p25", "p75"))
+      ) |>
+      replace_null_statistic(rows = !is.null(error)),
+    NA
+  )
+
+  # all results should now be NA_character
+  expect_equal(
+    ard_with_missing_stats$statistic |> unlist() |> unique(),
+    NA_character_
+  )
+})
