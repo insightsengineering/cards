ae360e9b2da5876ace3937c09c8d4f6f2f175804
Rohan Parmar <rohan.parmar@roche.com>
2024-04-30 20:02:58 +0000

    Update.

--- R/ard_attributes.R
+++ R/ard_attributes.R
@@ -5,20 +5,9 @@
 #'
 #' * `data.frame`: Adds variable attributes to an ARD data frame.
 #'
-#' Add variable attributes to an ARD data frame.
-#' - The `label` attribute will be added for all columns, and when no label
-#'   is specified and no label has been set for a column using the `label=` argument,
-#'   the column name will be placed in the label statistic.
-#' - The `class` attribute will also be returned for all columns.
-#' - Any other attribute returned by `attributes()` will also be added, e.g. factor levels.
-#'
 #' @param data (`data.frame`)\cr
 #'   a data frame
-#' @param variables ([`tidy-select`][dplyr::dplyr_tidy_select])\cr
-#'   variables to include
-#' @param label (named `list`)\cr
-#'   named list of variable labels, e.g. `list(cyl = "No. Cylinders")`.
-#'   Default is `NULL`
+#' @param ... Arguments passed to other methods
 #'
 #' @return an ARD data frame of class 'card'
 #'
@@ -35,7 +24,20 @@ ard_attributes <- function(data, ...) {
 }
 
 
+#' Add variable attributes to an ARD data frame.
+#' - The `label` attribute will be added for all columns, and when no label
+#'   is specified and no label has been set for a column using the `label=` argument,
+#'   the column name will be placed in the label statistic.
+#' - The `class` attribute will also be returned for all columns.
+#' - Any other attribute returned by `attributes()` will also be added, e.g. factor levels.
+#'
 #' @rdname ard_attributes
+#' @param variables ([`tidy-select`][dplyr::dplyr_tidy_select])\cr
+#'   variables to include
+#' @param label (named `list`)\cr
+#'   named list of variable labels, e.g. `list(cyl = "No. Cylinders")`.
+#'   Default is `NULL`
+#'
 #' @export
 ard_attributes.data.frame <- function(data,
                                       variables = everything(),
