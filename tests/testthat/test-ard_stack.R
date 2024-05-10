64a1883de047ebbff27c83a2a0a314a3859e357a
Daniel Sjoberg <danield.sjoberg@gmail.com>
2024-05-10 13:46:52 -0700

    Update test-ard_stack.R

--- tests/testthat/test-ard_stack.R
+++ tests/testthat/test-ard_stack.R
@@ -155,11 +155,9 @@ test_that("ard_stack() adding missing/attributes", {
       ard_continuous(data = mtcars, by = "cyl", variables = "mpg"),
       ard_dichotomous(data = mtcars, by = "cyl", variables = "vs"),
       ard_missing(data = mtcars, by = "cyl", variables = c("mpg", "vs")),
-
       ard_continuous(data = mtcars, variables = "mpg"),
       ard_dichotomous(data = mtcars, variables = "vs"),
       ard_missing(data = mtcars, variables = c("mpg", "vs")),
-
       ard_categorical(data = mtcars, variables = "cyl"),
       ard_attributes(mtcars, variables = c("cyl", "mpg", "vs")),
       .update = TRUE,
