900cb857c798868062411a8e702753615c9bd5a0
Emily de la Rua <emily.de_la_rua@contractors.roche.com>
2024-08-12 20:04:11 -0400

    Add `as_card` function

---
+++ R/as_card.R
@@ -0,0 +1,26 @@
+#' Data Frame as ARD
+#'
+#' Convert data frames to ARDs.
+#'
+#' @param x (`data.frame`)\cr
+#'   a data frame
+#'
+#' @return an ARD data frame of class 'card'
+#' @export
+#'
+#' @examples
+#' data.frame(
+#'   stat_name = c("N", "mean"),
+#'   stat_label = c("N", "Mean"),
+#'   stat = c(10, 0.5)
+#' ) |>
+#'   as_card()
+as_card <- function(x) {
+  set_cli_abort_call()
+
+  # check in inputs ------------------------------------------------------------
+  check_class(x, cls = "data.frame")
+
+  # convert to class "card" ----------------------------------------------------
+  structure(x, class = c("card", class(x)))
+}
