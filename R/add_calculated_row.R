18cace0704630e1216eb0d49052c6bb11780a21c
Daniel Sjoberg <danield.sjoberg@gmail.com>
2024-08-27 15:10:20 -0700

    Adding `add_calculated_row()` (#281)
    **What changes are proposed in this pull request?**
    * Added new function `add_calculated_row()` for adding a new row of
    calculated statistic(s) that are a function of the other statistics in
    the ARD. (#275)
    
    **Reference GitHub issue associated with pull request.** _e.g., 'closes
    #<issue number>'_
    closes #275
    
    
    --------------------------------------------------------------------------------
    
    Pre-review Checklist (if item does not apply, mark is as complete)
    - [ ] **All** GitHub Action workflows pass with a :white_check_mark:
    - [ ] PR branch has pulled the most recent updates from master branch:
    `usethis::pr_merge_main()`
    - [ ] If a bug was fixed, a unit test was added.
    - [ ] Code coverage is suitable for any new functions/features
    (generally, 100% coverage for new code): `devtools::test_coverage()`
    - [ ] Request a reviewer
    
    Reviewer Checklist (if item does not apply, mark is as complete)
    
    - [ ] If a bug was fixed, a unit test was added.
    - [ ] Run `pkgdown::build_site()`. Check the R console for errors, and
    review the rendered website.
    - [ ] Code coverage is suitable for any new functions/features:
    `devtools::test_coverage()`
    
    When the branch is ready to be merged:
    - [ ] Update `NEWS.md` with the changes from this pull request under the
    heading "`# cards (development version)`". If there is an issue
    associated with the pull request, reference it in parentheses at the end
    update (see `NEWS.md` for examples).
    - [ ] **All** GitHub Action workflows pass with a :white_check_mark:
    - [ ] Approve Pull Request
    - [ ] Merge the PR. Please use "Squash and merge" or "Rebase and merge".
    
    ---------
    
    Signed-off-by: Daniel Sjoberg <danield.sjoberg@gmail.com>
    Co-authored-by: Becca Krouse <14199771+bzkrouse@users.noreply.github.com>

---
+++ R/add_calculated_row.R
@@ -0,0 +1,98 @@
+#' Add Calculated Row
+#'
+#' @description
+#' Use this function to add a new statistic row that is a function of the
+#' other statistics in an ARD.
+#'
+#' @param x (`card`)\cr
+#'   data frame of class `'card'`
+#' @param expr (`expression`)\cr
+#'   an expression
+#' @param stat_name (`string`)\cr
+#'   string naming the new statistic
+#' @param by ([`tidy-select`][dplyr::dplyr_tidy_select])\cr
+#'   Grouping variables to calculate statistics within
+#' @param stat_label (`string`)\cr
+#'   string of the statistic label. Default is the `stat_name`.
+#' @param fmt_fn (`integer`, `function`, `string`)\cr
+#'   a function of an integer or string that can be converted to a function with
+#'  `alias_as_fmt_fn()`.
+#'
+#' @return an ARD data frame of class 'card'
+#' @export
+#'
+#' @examples
+#' ard_continuous(mtcars, variables = mpg) |>
+#'   add_calculated_row(expr = max - min, stat_name = "range")
+#'
+#' ard_continuous(mtcars, variables = mpg) |>
+#'   add_calculated_row(
+#'     expr =
+#'       dplyr::case_when(
+#'         mean > median ~ "Right Skew",
+#'         mean < median ~ "Left Skew",
+#'         .default = "Symmetric"
+#'       ),
+#'     stat_name = "skew"
+#'   )
+add_calculated_row <- function(x,
+                               expr,
+                               stat_name,
+                               by = c(all_ard_groups(), all_ard_variables(), any_of("context")),
+                               stat_label = stat_name,
+                               fmt_fn = NULL) {
+  set_cli_abort_call()
+  expr <- enexpr(expr)
+
+  # check inputs ---------------------------------------------------------------
+  check_not_missing(x)
+  check_not_missing(expr)
+  check_not_missing(stat_name)
+  check_class(x, "card")
+  check_string(stat_name)
+  check_string(stat_label)
+  process_selectors(x, by = {{ by }})
+
+  # calculate additional statistics --------------------------------------------
+  ard_calculated_stat <-
+    x |>
+    dplyr::group_by(dplyr::pick(any_of(by))) |>
+    dplyr::group_map(
+      \(x_subgroup, df_groups) {
+        if (any(duplicated(x_subgroup$stat_name))) {
+          cli::cli_abort(
+            "Duplicate statistics present within {.arg by} groups: {.val {x_subgroup$stat_name[duplicated(x_subgroup$stat_name)]}}",
+            call = get_cli_abort_call()
+          )
+        }
+
+        new_stat <-
+          eval_capture_conditions(
+            eval_tidy(expr, data = get_ard_statistics(x_subgroup))
+          )
+        if (!is_empty(new_stat[["error"]])) {
+          cli::cli_abort(
+            c("There was an error calculating the new statistic. See below:",
+              "x" = new_stat[["error"]]
+            ),
+            call = get_cli_abort_call()
+          )
+        }
+
+        df_groups |>
+          dplyr::mutate(
+            stat = list(.env$new_stat[["result"]]),
+            stat_name = .env$stat_name,
+            stat_label = .env$stat_label,
+            fmt_fn = list(fmt_fn %||% ifelse(is.numeric(new_stat[["result"]]), 1L, as.character))
+          )
+      }
+    ) |>
+    dplyr::bind_rows()
+
+  # stack passed ARD and new ARD stats -----------------------------------------
+  dplyr::bind_rows(
+    x,
+    ard_calculated_stat
+  )
+}
