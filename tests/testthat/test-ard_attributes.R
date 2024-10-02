67522720c7be70e62351cf1667bbf6bafdcf5258
Jack Talboys <42234641+jtalboys@users.noreply.github.com>
2024-10-02 05:08:06 +0100

    Add columns `fmt_fn`, `warning`, and `errors` to `ard_attributes` output (#343)
    **What changes are proposed in this pull request?**
    * Add columns `fmt_fn`, `warning`, and `errors` to `ard_attributes()`
    output (#327)
    
    Provide more detail here as needed.
    
    closes #327
    
    Please let me know if there is anything else to change here. I'm not
    100% sure I'm setting the `fmt_fn` column right? Also had a look at the
    docs for `ard_attributes()` to see if any changes were required but they
    only talk about the added attributes, don't mention the columns of the
    output.
    
    
    --------------------------------------------------------------------------------
    
    Pre-review Checklist (if item does not apply, mark is as complete)
    - [x] **All** GitHub Action workflows pass with a :white_check_mark:
    - [x] PR branch has pulled the most recent updates from master branch:
    `usethis::pr_merge_main()`
    - [x] If a bug was fixed, a unit test was added.
    - [x] Code coverage is suitable for any new functions/features
    (generally, 100% coverage for new code): `devtools::test_coverage()`
    - [x] Request a reviewer
    
    Reviewer Checklist (if item does not apply, mark is as complete)
    
    - [x] If a bug was fixed, a unit test was added.
    - [x] Run `pkgdown::build_site()`. Check the R console for errors, and
    review the rendered website.
    - [x] Code coverage is suitable for any new functions/features:
    `devtools::test_coverage()`
    
    When the branch is ready to be merged:
    - [x] Update `NEWS.md` with the changes from this pull request under the
    heading "`# cards (development version)`". If there is an issue
    associated with the pull request, reference it in parentheses at the end
    update (see `NEWS.md` for examples).
    - [x] **All** GitHub Action workflows pass with a :white_check_mark:
    - [x] Approve Pull Request
    - [x] Merge the PR. Please use "Squash and merge" or "Rebase and merge".
    
    ---------
    
    Co-authored-by: Daniel Sjoberg <danield.sjoberg@gmail.com>

--- tests/testthat/test-ard_attributes.R
+++ tests/testthat/test-ard_attributes.R
@@ -1,4 +1,7 @@
+skip_if_not(is_pkg_installed("withr"))
+
 test_that("ard_attributes() works", {
+  withr::local_options(list(width = 120))
   expect_snapshot({
     df <- dplyr::tibble(var1 = letters, var2 = LETTERS)
     attr(df$var1, "label") <- "Lowercase Letters"
@@ -12,5 +15,21 @@ test_that("ard_attributes() errors when there is no dataframe", {
   expect_error(
     ard_attributes("test"),
     "There is no method for objects of class <character>."
+  )
+})
+
+test_that("ard_attributes() follows ard structure", {
+  expect_silent(
+    ard_attributes(ADSL[c("AGE", "AGEGR1")]) |>
+      check_ard_structure(method = FALSE)
+  )
+})
+
+test_that("ard_attributes() requires label as a named list", {
+  expect_snapshot(
+    error = TRUE,
+    ard_attributes(ADSL[c("AGE", "AGEGR1")],
+      label = list("test")
+    )
   )
 })
