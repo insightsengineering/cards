9ad0dad456340d11efbd676fcc62198371fa92ad
Emily de la Rua <emily.de_la_rua@contractors.roche.com>
2025-01-15 14:59:38 -0500

    Update denominator argument description

--- R/ard_hierarchical.R
+++ R/ard_hierarchical.R
@@ -18,12 +18,19 @@
 #' @param id ([`tidy-select`][dplyr::dplyr_tidy_select])\cr
 #'   an optional argument used to assert there are no duplicates within
 #'   the `c(id, variables)` columns.
+#' @param denominator (`data.frame`, `integer`)\cr
+#'   used to define the denominator and enhance the output.
+#'   The argument is required for `ard_hierarchical()` and optional
+#'   for `ard_hierarchical_count()`.
+#'   - the univariate tabulations of the `by` variables are calculated with `denominator`,
+#'     when a data frame is passed, e.g. tabulation of the treatment assignment
+#'     counts that may appear in the header of a table.
+#'   - the `denominator` argument must be specified when `id` is used to
+#'     calculate the event rates.
 #' @inheritParams ard_categorical
 #'
 #' @return an ARD data frame of class 'card'
 #' @name ard_hierarchical
-#'
-#' @inheritSection ard_categorical Denominators
 #'
 #' @examples
 #' ard_hierarchical(
