16b1c51a3e8d048360baa625317aa3671940e96f
Emily de la Rua <emily.de_la_rua@contractors.roche.com>
2025-02-10 19:46:37 -0500

    Allow filtering to use by variables

--- R/ard_filter.R
+++ R/ard_filter.R
@@ -12,16 +12,19 @@
 #' @details
 #' The `filter` argument can be used to filter out rows of a hierarchical ARD which do not meet the requirements
 #' provided as an expression. Rows can be filtered on the values of any of the possible statistics (`n`, `p`, and `N`)
-#' provided they are included at least once in the ARD. For each entry that does not meet the filtering requirement,
-#' all statistics corresponding to that entry will be removed from the ARD. In addition to filtering on individual
-#' statistic values, filters can be applied across the hierarchical row (i.e. across all `by` variable values) by using
-#' aggregate functions such as `sum()` and `mean()`.
+#' provided they are included at least once in the ARD, as well as the values of any `by` variables. For each entry that
+#' does not meet the filtering requirement, all statistics corresponding to that entry will be removed from the ARD.
+#' Filtering is only applied to rows that correspond to the innermost variable in the hierarchy - all outer variable
+#' (summary) rows will be kept. In addition to filtering on individual statistic values, filters can be applied across
+#' the hierarchical row (i.e. across all `by` variable values) by using aggregate functions such as `sum()` and
+#' `mean()`.
 #'
 #' Some examples of possible filters:
 #' - `filter = n > 5`
 #' - `filter = n == 2 & p < 0.05`
 #' - `filter = sum(n) > 4`
 #' - `filter = mean(n) > 4 | n > 3`
+#' - `filter = any(n > 2 & TRTA == "Xanomeline High Dose")`
 #'
 #' @return an ARD data frame of class 'card'
 #' @seealso [ard_sort()]
@@ -60,12 +63,12 @@ ard_filter <- function(x, filter) {
       call = get_cli_abort_call()
     )
   }
-  if (!all(all.vars(filter) %in% x$stat_name)) {
-    var_miss <- setdiff(all.vars(filter), x$stat_name)
+  if (!all(all.vars(filter) %in% c(x$stat_name, attributes(x)$args$by))) {
+    var_miss <- setdiff(all.vars(filter), c(x$stat_name, attributes(x)$args$by))
     cli::cli_abort(
       paste(
-        "The expression provided as {.arg filter} includes condition{?s} for statistic{?s} {.val {var_miss}} which",
-        "{?is/are} not present in the ARD."
+        "The expression provided as {.arg filter} includes condition{?s} for statistic{?s} or `by` variable{?s}",
+        "{.val {var_miss}} which {?is/are} not present in the ARD."
       ),
       call = get_cli_abort_call()
     )
@@ -79,8 +82,8 @@ ard_filter <- function(x, filter) {
     dplyr::select(all_ard_groups(), all_ard_variables(), "stat_name", "stat", "idx") |>
     tidyr::pivot_wider(
       id_cols = c(all_ard_groups(), all_ard_variables()),
-      names_from = stat_name,
-      values_from = stat,
+      names_from = "stat_name",
+      values_from = "stat",
       values_fn = unlist,
       unused_fn = list
     )
@@ -89,6 +92,9 @@ ard_filter <- function(x, filter) {
   f_idx <- x_f |>
     dplyr::group_by(across(c(all_ard_groups(), all_ard_variables(), -by_cols))) |>
     dplyr::group_map(\(.df, .g) {
+      # allow filtering on by variable levels
+      names(.df)[names(.df) == by_cols[c(FALSE, TRUE)]] <- .df[1, names(.df)[names(.df) == by_cols[c(TRUE, FALSE)]]]
+
       # only filter rows for innermost variable
       if (.g$variable == dplyr::last(attributes(x)$args$variables)) {
         .df[["idx"]][eval_tidy(filter, data = .df)]
