083adcacb105c93c8c7909b513d911fcd216c742
Daniel Sjoberg <danield.sjoberg@gmail.com>
2025-03-24 09:32:37 -0700

    First draft complete

--- tests/testthat/test-shift_ard_columns.R
+++ tests/testthat/test-shift_ard_columns.R
@@ -1,8 +1,11 @@
+skip_if_not(is_pkg_installed("withr"))
+
 test_that("rename_ard_columns() works", {
+  withr::local_options(width = 220)
   # no variable level
   res_var <- ard_continuous(data = ADSL, by = TRT01A, variables = c(AGE, BMIBL, HEIGHTBL))
 
-  res_rnm_var <- rename_ard_columns(res_var, all_ard_variables())
+  res_rnm_var <- rename_ard_columns(res_var, all_ard_variables("names"))
 
   expect_equal(nrow(res_var), nrow(res_rnm_var))
 
@@ -20,11 +23,11 @@ test_that("rename_ard_columns() works", {
     ard_continuous(variables = AGE)
   )
 
-  res_rnm_multi <- rename_ard_columns(res_multi, all_ard_variables())
+  res_rnm_multi <- rename_ard_columns(res_multi, all_ard_variables("names"))
 
   expect_equal(nrow(res_multi), nrow(res_rnm_multi))
 
-  res_multi_1 <- rename_ard_columns(res_multi, c(all_ard_groups(), all_ard_variables()))
+  res_multi_1 <- rename_ard_columns(res_multi, c(all_ard_groups("names"), all_ard_variables("names")))
 
   # rename groups and variables
   expect_snapshot(
@@ -38,36 +41,6 @@ test_that("rename_ard_columns() works", {
   expect_equal(
     res_multi_1,
     res_multi |>
-      rename_ard_columns(c(
-        variable, group1, group2_level,
-        variable_level, group2, group1_level
-      ))
-  )
-
-  # rename variables after shuffle
-  # include case where one of the variables is already present as a group
-  res_shuffle <- ard_stack(
-    data = ADSL,
-    .by = TRT01A,
-    ard_categorical(variables = c(RACE, ETHNIC)),
-    ard_continuous(variables = AGE),
-    .shuffle = TRUE
-  )
-
-  expect_snapshot(
-    res_shuffle |>
-      rename_ard_columns(c(variable, label)) |>
-      dplyr::slice(1:20) |>
-      as.data.frame()
-  )
-
-  # _level doesn't have a match
-  res_var <- ard_continuous(data = ADSL, by = TRT01A, variables = c(AGE, BMIBL, HEIGHTBL))
-
-  expect_snapshot(
-    rename_ard_columns(
-      res_var |> dplyr::select(-group1),
-      "group1_level"
-    )
+      rename_ard_columns(c(variable, group1, group2))
   )
 })
