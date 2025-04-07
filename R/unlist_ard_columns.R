30ec8a5c6a60cab9f507216827dc8a1b6c608ec5
Daniel Sjoberg <danield.sjoberg@gmail.com>
2025-04-07 09:34:54 -0700

    Adding option to convert fct to chr

--- R/unlist_ard_columns.R
+++ R/unlist_ard_columns.R
@@ -8,6 +8,12 @@
 #' @param fill (scalar)\cr
 #'   scalar to fill NULL values with before unlisting (if they are present).
 #'   Default is `NA`.
+#' @param fct_as_chr (scalar `logical`)\cr
+#'   When `TRUE`, factor elements will be converted to character before unlisting.
+#'   When the column being unlisted contains mixed types of classes, the
+#'   factor elements are often converted to the unerlying interger value instead
+#'   of retaining the label. Default is `TRUE`.
+#'
 #'
 #' @returns a data frame
 #' @export
@@ -24,18 +30,36 @@
 #'   unlist_ard_columns()
 unlist_ard_columns <- function(x,
                                columns = c(where(is.list), -any_of(c("warning", "error", "fmt_fn"))),
-                               fill = NA) {
+                               fill = NA,
+                               fct_as_chr = TRUE) {
   # check inputs ---------------------------------------------------------------
   set_cli_abort_call()
   check_data_frame(x)
   process_selectors(x, columns = {{ columns }})
   check_scalar(fill)
+  check_scalar_logical(fct_as_chr)
 
   # first replace any NULL values with the fill value --------------------------
-  x <- x |>
-    dplyr::mutate(
-      across(all_of(columns), ~ map(., \(value) value %||% .env$fill))
-    )
+  if (isTRUE(fct_as_chr)) {
+    x <- x |>
+      dplyr::mutate(
+        across(
+          all_of(columns),
+          ~ map(., \(value) {
+            if (inherits(value, "factor")) value <- as.character(value) # styler: off
+            value %||% .env$fill
+          }
+          )
+        )
+      )
+  }
+  else {
+    x <- x |>
+      dplyr::mutate(
+        across(all_of(columns), ~ map(., \(value) value %||% .env$fill))
+      )
+  }
+
 
   # unlist the columns ---------------------------------------------------------
   for (var in columns) {
