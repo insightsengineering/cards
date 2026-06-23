64637d82238c132ad63a7fc2cd8604eb7266c68a
Alanah Jonas <156101954+alanahjonas95@users.noreply.github.com>
2026-06-23 15:21:39 +0100

    add test to check class is assigned

--- tests/testthat/test-bind_ard.R
+++ tests/testthat/test-bind_ard.R
@@ -1,10 +1,15 @@
 test_that("bind_ard() works", {
   ard <- ard_tabulate(ADSL, by = "ARM", variables = "AGEGR1")
+  res <- bind_ard(ard, ard, .update = TRUE)
 
   expect_error(
-    bind_ard(ard, ard, .update = TRUE),
+    res,
     NA
   )
+
+  # Verify the new class is assigned and ordered correctly
+  expect_s3_class(res, "bind_ard")
+  expect_identical(class(res)[1], "bind_ard")
 })
 
 
